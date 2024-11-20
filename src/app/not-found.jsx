10ac8c261c7dd1a3eddbe8b7dca49aacb2fb8495
masiham <masihmooohmmadpoor@gmail.com>
2024-11-20 10:42:20 +0330

    created 404 page

---
+++ src/app/not-found.jsx
@@ -0,0 +1,18 @@
+"use client";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+const NotFound = () => {
+  const router = useRouter();
+  return (
+    <section className="flex flex-col justify-center items-center gap-y-4">
+      <Image src="/images/404.svg" width={300} height={100} alt="404" />
+      <h1 className="font-semibold text-xl">صفحه مورد نظر پیدا نشد!</h1>
+      <button onClick={() => router.back()} className="btn btn--primary">
+        برگشت به صفحه اصلی
+      </button>
+    </section>
+  );
+};
+
+export default NotFound;
